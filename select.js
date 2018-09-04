f7ff5c80bc5a40fb8b22675e6938e6b90414c107
bob-perez-utah <bperez@gw.novell.com>
2018-09-03 21:28:24 -0600

    final commit

---
+++ select.js
@@ -0,0 +1,9 @@
+document.getElementById("first");
+
+document.getElementsByClassName("special") [0];
+
+document.getElementsByTagName("p") [0];
+
+document.querySelector("#first");
+
+document.querySelectorAll("#first") [0];
